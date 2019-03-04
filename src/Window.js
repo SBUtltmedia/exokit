73f7348640478ade664639a372f10d59633f2fac
Avaer Kazmer <a@modules.io>
2019-03-04 10:00:08 -0500

    Small Window.js xrState cleanup

--- src/Window.js
+++ src/Window.js
@@ -545,7 +545,7 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
     webkitGetUserMedia: getUserMedia, // for feature detection
     getVRDisplaysSync() {
       const result = [];
-      if (GlobalContext.xrState.fakeVrDisplayEnabled[0]) {
+      if (xrState.fakeVrDisplayEnabled[0]) {
         result.push(window[symbols.mrDisplaysSymbol].fakeVrDisplay);
       }
       if (nativeMl && nativeMl.IsPresent()) {
@@ -565,7 +565,7 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
       return result;
     },
     createVRDisplay() {
-      GlobalContext.xrState.fakeVrDisplayEnabled[0] = 1;
+      xrState.fakeVrDisplayEnabled[0] = 1;
       return window[symbols.mrDisplaysSymbol].fakeVrDisplay;
     },
     getGamepads: getGamepads.bind(null, window),
