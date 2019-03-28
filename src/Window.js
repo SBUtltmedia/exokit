7be3eb15048b2e0a1f0fcaf7c0e4cdfc2fb0042d
Avaer Kazmer <a@modules.io>
2019-03-28 02:51:12 +0000

    More framebuffer copy save to directly after local render

--- src/Window.js
+++ src/Window.js
@@ -1176,6 +1176,11 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
           }
         }
       }
+
+      if (window.document.framebuffer) {
+        const {msFbo, msTex, msDepthTex, copyMsFbo, copyMsTex, copyMsDepthTex} = window.document.framebuffer;
+        nativeWindow.copyRenderTarget(window.document.framebufferContext, window.document.framebufferContext.width, window.document.framebufferContext.height, msFbo, msTex, msDepthTex, copyMsFbo, copyMsTex, copyMsDepthTex);
+      }
     };
     const _renderChildren = async () => {
       let timeout;
@@ -1705,11 +1710,6 @@ const _normalizeUrl = utils._makeNormalizeUrl(options.baseUrl);
           }
         }
       }
-    }
-
-    if (window.document.framebuffer) {
-      const {msFbo, msTex, msDepthTex, copyMsFbo, copyMsTex, copyMsDepthTex} = window.document.framebuffer;
-      nativeWindow.copyRenderTarget(window.document.framebufferContext, window.document.framebufferContext.width, window.document.framebufferContext.height, msFbo, msTex, msDepthTex, copyMsFbo, copyMsTex, copyMsDepthTex);
     }
 
     for (let i = 0; i < windows.length; i++) {
